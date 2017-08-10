cbd946203ed8256bc7bf8ce5af4811c7fe2d25ba
Carmen Lucía Barrios Benavides <carmenluciabarrios@hotmail.com>
2017-08-10 10:56:28 -0500

    Update ListaInversa.java

--- src/taller1/ListaInversa.java
+++ src/taller1/ListaInversa.java
@@ -24,14 +24,27 @@ public class ListaInversa {
                 array[i][j] = scan.nextInt();
             }
         }
+        int[][] nuevo= new int [testCases][100000];
         for (int i=0; i<testCases; i++){
+            System.out.println("i" + i);
             for (int j=0; j< n[i]; j++){
+                System.out.println("j" + j);
+                nuevo[i][array[i][j]]=j+1;
                 System.out.println(array[i][j]);
+                System.out.println(nuevo[i][array[i][j]]);
             }
         }
         
         
-        
+        for (int i=0; i<testCases; i++){
+            int p=0;
+            for (int j=1; j<(n[i] +1); j++){
+                if (array[j][i]==nuevo[i][j]){
+                    p=p+1;
+                }
+            }
+            if (p==)
+        }
         
         
     }
